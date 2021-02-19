10d7a8ea3258a21c1697c69132d096aabcd0e605
3122094109@qq.com <tqw970102>
2021-02-19 23:46:05 +0800

    add findSearch method by jpa.

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -7,34 +7,45 @@ import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/label")
 public class LabelController {
     @Autowired
     private LabelService labelService;
+
     @GetMapping
     public Result findAll() {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
+
     @GetMapping("/{labelId}")
-    public  Result findById(@PathVariable String labelId){
+    public Result findById(@PathVariable String labelId) {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
+
     @PostMapping
-    public Result save(@RequestBody Label label){
+    public Result save(@RequestBody Label label) {
         labelService.save(label);
-        return new Result(true,StatusCode.OK, "添加成功");
+        return new Result(true, StatusCode.OK, "添加成功");
     }
+
     @PutMapping("/{labelId}")
-    public Result update(@PathVariable String id, @RequestBody Label label){
+    public Result update(@PathVariable String id, @RequestBody Label label) {
         label.setId(id);
         labelService.update(label);
-        return new Result(true,StatusCode.OK, "更改成功");
+        return new Result(true, StatusCode.OK, "更改成功");
     }
+
     @DeleteMapping("/{labelId}")
-    public Result deleteById(@PathVariable String labelId){
+    public Result deleteById(@PathVariable String labelId) {
         labelService.deleteById(labelId);
-        return new Result(true,StatusCode.OK, "删除成功");
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+    public Result findSearch(@RequestBody Label label){
+        List<Label> list = labelService.findSearch(label);
+        return new Result(true, StatusCode.OK, "查询成功", list);
     }
 }
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,10 +3,16 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,4 +45,23 @@ public class LabelService {
     }
 
 
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(new Specification<Label>(){
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                List<Predicate> predicates = new ArrayList<>();
+                if(label.getLabelname()!=null&&!label.getLabelname().equals("")){
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class),"%"+label.getLabelname()+"%");
+                    predicates.add(predicate);
+                }
+                if(label.getState()!=null&&!label.getState().equals("")){
+                    Predicate predicate = cb.equal(root.get("state").as(String.class),label.getState());
+                    predicates.add(predicate);
+                }
+                Predicate[] list = new Predicate[predicates.size()];
+                predicates.toArray(list);
+                return cb.and(list);
+            }
+        });
+    }
 }
